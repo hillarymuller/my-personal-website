4e1cc1259f7474834dabc262b7e943c51cbfb5eb
Hillary Muller <hillaryaward@gmail.com>
2022-02-21 15:13:44 -0500

    added mouseover, linked interests, and added paragraph

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+let menu = document.getElementById("menu");
+menu.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "pink";
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 500);
+}, false);
